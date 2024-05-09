68b9aa699e4f8053c2f4166903d1c11aaa09d3b9
Joseph home <joseph122792@gmail.com>
2024-05-10 04:48:30 +0800

    Add boss timer. Let boss do something when timer down to 0

--- Source/Game/Boss.cpp
+++ Source/Game/Boss.cpp
@@ -3,10 +3,28 @@
 
 int Boss::getHealth()
 {
-    return this->health;
+	return this->health;
+}
+
+int Boss::getTimer()
+{
+	return this->timer;
 }
 
 void Boss::setHealth(int health)
 {
-    this->health = health;
+	this->health = health;
+}
+
+void Boss::setTimer(int timer)
+{
+	this->timer = timer;
+}
+
+void Boss::countdownTimer()
+{
+	if (this->timer > 0)
+	{
+		this->timer -= 1;
+	}
 }
