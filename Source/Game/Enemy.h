9dd0df938bf5e66647d411b8e3dcc46565bcf664
Joseph home <joseph122792@gmail.com>
2024-05-31 01:28:42 +0800

    add hitable member

--- Source/Game/Enemy.h
+++ Source/Game/Enemy.h
@@ -4,15 +4,22 @@ class Enemy :public MovingObject
 {
 private:
 	size_t frameCounter = 0;
+	bool hitable = true;
 	vector<POINTF> speeds;
-	map<size_t, vector<void(*)(Enemy, MovingObject*, vector<MovingObject>*)>> action;
+	map<size_t, vector<void(*)(Enemy*, MovingObject*, vector<MovingObject>*)>> action;
 	void updateBySpeeds();
 	void callEnemyAction(MovingObject* player, vector<MovingObject>* bullets);
 
 public:
+	// setter
 	void setSpeeds(vector<POINTF> speeds);
-	void setAction(map<size_t, vector<void(*)(Enemy, MovingObject*, vector<MovingObject>*)>> action);
-	void update(MovingObject *player, vector<MovingObject>* bullets);
+	void setAction(map<size_t, vector<void(*)(Enemy*, MovingObject*, vector<MovingObject>*)>> action);
+	void setHitable(bool hitable);
+
+	// getter
+	bool getHitable();
+
+	void update(MovingObject* player, vector<MovingObject>* bullets);
 
 };
 
