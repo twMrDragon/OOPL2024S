6db9ab0e30effa9131d3bb1eb71fef82cd2033fc
Joseph home <joseph122792@gmail.com>
2024-05-31 01:31:43 +0800

    tidy up code

--- Source/Game/RumiaFirst.h
+++ Source/Game/RumiaFirst.h
@@ -9,36 +9,30 @@ class RumiaFirst :public Boss
 public:
 	RumiaFirst();
 
-	enum class Action
-	{
-		ENTER,
-		ATTACK,
-		LEAVE,
-	};
-
-	void update(MovingObject player, vector<MovingObject>* enemyBullets) override;
+	void update(MovingObject* player, vector<MovingObject>* enemyBullets, MovingObject* playerArea) override;
 	void onInit(MovingObject playerArea) override;
 	void show() override;
-	void attack(MovingObject player, vector<MovingObject>* enemyBullets);
+	void attack(MovingObject* player, vector<MovingObject>* enemyBullets);
+
 
 private:
-	size_t frameCounter = 0;
-
-	Action currentAction = Action::ENTER;
+	// enter
 	vector<POINTF> enterSpeeds;
-	vector<POINTF> attackSpeeds;
-	vector<POINTF> leaveSpeeds;
 
-	// 發射 n 個子彈
-	void fireCircleShpaeNBullets(double angle, int n, vector<string> resource, float speed, vector<MovingObject>* enemyBullets);
+	// attack
+	// only one stage
+	vector<POINTF> attackSpeeds;
+
+	// leave
+	vector<POINTF> leaveSpeeds;
 
 	// 招式 1
 	// 發射 3 層同色子彈，一層 16 顆
-	void fire3LevelBullets(MovingObject player, vector<string> resource, vector<MovingObject>* enemyBullets);
+	void fire3LevelBullets(MovingObject* player, vector<string> resource, vector<MovingObject>* enemyBullets);
 
 	// 招式 2
 	// 發射 5 種顏色子彈，每種 14 顆
-	void fire5ColorBullets(MovingObject player, vector<MovingObject>* enemyBullets);
+	void fire5ColorBullets(MovingObject* player, vector<MovingObject>* enemyBullets);
 
 	// 招式 3
 	// 不同時間發射不同顏色不同形狀子彈
