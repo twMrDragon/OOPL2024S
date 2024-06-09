09bb000610c4878f2c4c68ef56a50a0645d8f670
Joseph home <joseph122792@gmail.com>
2024-06-09 22:03:17 +0800

    add calculate angle from source to target pointf

--- Source/Game/Utils.cpp
+++ Source/Game/Utils.cpp
@@ -7,3 +7,9 @@ POINTF Utils::calculateXYSpeed(double angle, float speed)
 	float y = (float)sin(angle * M_PI / 180) * speed;
 	return POINTF{ x,y };
 }
+
+double Utils::angleToTarget(POINTF source, POINTF target)
+{
+	double angle2Target = 180 - atan2(target.y - source.y, source.x - target.x) * 180 / M_PI;
+	return angle2Target;
+}
