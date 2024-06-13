f2036556d871fc3139979e074408cbe02ce15057
Joseph home <joseph122792@gmail.com>
2024-06-14 00:31:06 +0800

    let onShow be virtual function

--- Source/Game/Player.h
+++ Source/Game/Player.h
@@ -1,32 +1,40 @@
 #pragma once
 #include "MovingObject.h"
-class Player:public MovingObject
+class Player :public MovingObject
 {
-private:
+protected:
 	int invincible = 0;//NO Enemy ³o¬OµL¼Ä´V
 	int damage = 0;
-	int bomb=3;
-	int remainingLives=3;
+	int bomb = 3;
+	int power = 0;
+	int remainingLives = 3;
 	bool fire = false;//get set
-	MovingObject body;
-	void updateBodyLaction();
-	int power = 0;//get set
+	MovingObject hitbox;
+
+	void initHitbox();
+	void updateHitboxLocation();
+
 public:
-	void onInit();
-	void setPower(int power);
-	int getPower();
-	void setRemainingLives(int remainingLives);
-	int getRemainingLives();
-	void setInvincible(int invincible);
-	int getInvincible();//player show
-
-
+	// need to override
+	virtual void onInit() = 0;
 	virtual vector<MovingObject> attack() = 0;
+	virtual void onShow() = 0;
+	virtual void spellCard() = 0;
+
+	// setter
+	void setPower(int power);
+	void setRemainingLives(int remainingLives);
+	void setInvincible(int invincible);
+
+	// getter
+	int getPower();
+	int getRemainingLives();
+	int getInvincible();
+
 	bool isDeath(MovingObject bullet);
 	void reduceInvincible();
 
-	virtual void spellCard() = 0;
-
+	// will call parent function, because child class may have some other thing need to move
 	void setLocationF(POINTF location);
 	void setLocationF(float x, float y);
 	void updateLocationFBySpeed();
